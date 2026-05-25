eddb8fd7f66b314544b9808b93765b33004755a4
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-25 20:17:17 +0530

    shellscript

---
+++ Shell3.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USER=$(id -u)
+if [ $USER -ne 0 ]; then
+echo " Please run the script with root user"
+exit 1
+fi
+
+
+#installing mysql
+
+
+#Checking MYSQL package insatlled or not
+dpkg -l | grep mysql
+
+if [ $? -eq 0 ]; then
+echo " MYSQL already installed"
+exit 0
+else
+echo " MYSQL is not installed, Hence we are installing now"
+apt install mysql-server -y
+Validatepackage $? mysql
+fi
+
+
+Validatepackage()
+if [ $1 -eq 0 ]; then
+echo "$2 package installation is successfully"
+echo " Validating $2 service status now"
+Validateservice $2
+else
+echo "$2 Package installation is failed"
+exit 1
+fi
+
+Validateservice()
+Systemctl status $1
+if [ $? -eq 0 ]; then
+echo " $1 service has started "
+else
+echo " $1 service is not started, so manually starting and enabling the service "
+sudo systemctl enable --now mysql
+fi
