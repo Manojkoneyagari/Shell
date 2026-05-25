bab1052e9997e026a350bd204e5377d372a75a7c
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-25 21:23:29 +0530

    shellscript

--- Shell3.sh
+++ Shell3.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+Logfile=/opt/$0.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 USER=$(id -u)
 if [ $USER -ne 0 ]; then
 echo " Please run the script with root user"
@@ -11,22 +18,25 @@ fi
 
 Validatepackage() {
 if [ $1 -eq 0 ]; then
-echo "$2 package installation is successfully"
-echo " Validating $2 service status now"
+echo "$(date +%Y/%m/%d-%H:%M) $2 $G package installation is success $N"
+echo " $Y Validating $2 service status now $N"
 Validateservice $2
 else
-echo "$2 Package installation is failed"
+echo "$R $2 Package installation is failed $N"
 exit 1
 fi
 }
 
 Validateservice() {
-systemctl status $1
-if [ $? -eq 0 ]; then
-echo " $1 service has started "
-else
-echo " $1 service is not started, so manually starting and enabling the service "
-fi
+    if [ $1 = "mysql-server" ]; then
+        $1=mysql
+    fi
+        systemctl status $1
+      if [ $? -eq 0 ]; then
+      echo "$G $1 service has started $N"
+      else
+      echo "$R $1 service is not started, so manually starting and enabling the service $N"
+      fi
 }
 
 
@@ -36,11 +46,11 @@ for package in $@
 do
 dpkg -l | grep $package
 if [ $? -eq 0 ]; then
-echo " $package already installed"
+echo "$(date +%Y/%m/%d-%H:%M) $package already $G installed $N"
 exit 0
 else
-echo " $package is not installed, Hence we are installing now"
-apt install $package -y
+echo "$(date +%Y/%m/%d-%H:%M) $package is not installed, Hence we are $Y installing now $N" | tee -a Logfile
+apt install $package -y &>> Logfile
 Validatepackage $? $package
 fi
 done
--- removal.sh
+++ removal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-if [ $1 -eq "mysql" ]; then
+if [ $1 = "mysql" ]; then
 systemctl stop mysql
 apt purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-* -y
 apt autoremove -y
@@ -10,4 +10,17 @@ sudo rm -rf /var/lib/mysql
 sudo rm -rf /var/log/mysql
 dpkg -l | grep mysql
 echo "status of mysql package uninstallation $?"
+
+elif [ $1 = "nginx" ]; then
+sudo systemctl stop nginx
+sudo apt purge nginx nginx-common nginx-core -y
+sudo apt autoremove -y
+sudo rm -rf /etc/nginx
+sudo rm -rf /var/log/nginx
+sudo rm -rf /var/www/html
+dpkg -l | grep nginx
+echo "status of nginx package uninstallation $?"
+
+else
+echo " give proper argument in command line"
 fi
