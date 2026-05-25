280bb5625b8048ad6d96199634480fc5a0fc3c78
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-25 21:27:11 +0530

    shellscript

--- removal.sh
+++ removal.sh
@@ -2,6 +2,8 @@
 
 
 if [ $1 = "mysql" ]; then
+systemctl status $1
+   if [ $? -eq 0 ]; then
 systemctl stop mysql
 apt purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-* -y
 apt autoremove -y
@@ -10,8 +12,13 @@ sudo rm -rf /var/lib/mysql
 sudo rm -rf /var/log/mysql
 dpkg -l | grep mysql
 echo "status of mysql package uninstallation $?"
+else
+echo " $1 service is already in stopped state"
+fi
 
 elif [ $1 = "nginx" ]; then
+systemctl status $1
+   if [ $? -eq 0 ]; then
 sudo systemctl stop nginx
 sudo apt purge nginx nginx-common nginx-core -y
 sudo apt autoremove -y
@@ -20,6 +27,8 @@ sudo rm -rf /var/log/nginx
 sudo rm -rf /var/www/html
 dpkg -l | grep nginx
 echo "status of nginx package uninstallation $?"
+else
+echo " $1 service is already in stopped state"
 
 else
 echo " give proper argument in command line"
