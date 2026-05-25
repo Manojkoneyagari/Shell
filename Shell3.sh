62c34d10cd7a6652a786b893b5e6a009380d8023
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-25 21:41:59 +0530

    shellscript

--- Shell3.sh
+++ Shell3.sh
@@ -18,24 +18,25 @@ fi
 
 Validatepackage() {
 if [ $1 -eq 0 ]; then
-echo "$(date +%Y/%m/%d-%H:%M) $2 $G package installation is success $N"
-echo " $Y Validating $2 service status now $N"
+echo "$(date +%Y/%m/%d-%H:%M) $2 ${G} package installation is success ${N}"
+echo " ${Y} Validating $2 service status now ${N}"
 Validateservice $2
 else
-echo "$R $2 Package installation is failed $N"
+echo "${R} $2 Package installation is failed ${N}"
 exit 1
 fi
 }
 
 Validateservice() {
+    Service=$1
     if [ $1 = "mysql-server" ]; then
-        $1=mysql
+        Service="mysql"
     fi
-        systemctl status $1
+        systemctl status $Service
       if [ $? -eq 0 ]; then
-      echo "$G $1 service has started $N"
+      echo "${G} $Service service has started ${N}"
       else
-      echo "$R $1 service is not started, so manually starting and enabling the service $N"
+      echo "${R} $Service service is not started, so manually starting and enabling the service ${N}"
       fi
 }
 
@@ -46,10 +47,10 @@ for package in $@
 do
 dpkg -l | grep $package
 if [ $? -eq 0 ]; then
-echo "$(date +%Y/%m/%d-%H:%M) $package already $G installed $N"
+echo "$(date +%Y/%m/%d-%H:%M)- $package already ${G} installed ${N}"
 exit 0
 else
-echo "$(date +%Y/%m/%d-%H:%M) $package is not installed, Hence we are $Y installing now $N" | tee -a Logfile
+echo "$(date +%Y/%m/%d-%H:%M) $package is not installed, Hence we are ${Y} installing now ${N}" | tee -a Logfile
 apt install $package -y &>> Logfile
 Validatepackage $? $package
 fi
