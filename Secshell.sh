e7947c5797d403e33cc434b77733e200afbfeb63
ManojKoneyagari <koneyagarimanojkumar@gmail.com>
2026-05-25 20:24:43 +0530

    shellscript

--- Secshell.sh
+++ Secshell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-USER=$(whoami)
+USER=$(id -u)
 
-if [ $USER != "root" ]; then
+if [ $USER -ne 0 ]; then
 echo " Please run the script with root user"
 else
 echo "This script runned by root user and date is $(date)"
